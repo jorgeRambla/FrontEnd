47c7eca15f216c3e495e4421d36003ad40da88fa
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2020-03-10 19:07:52 +0100

    Just remove dup hidding bug

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ import {LogLevel} from '../app/services/shared/LogLevel';
 
 export const environment = {
   production: true,
-  baseAPIUrl: 'https://https://pre-murcy-api.herokuapp.com/api/',
+  baseAPIUrl: 'https://pre-murcy-api.herokuapp.com/api/',
   logger: {
     level: LogLevel.Error,
     printDate: true
