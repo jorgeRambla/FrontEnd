4417b3e3a757a972f8a793e5537e4a83e31ae5f0
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2020-03-10 19:04:37 +0100

    responsive sidenav

--- src/app/components/privileged-user/sidenav-nav/sidenav-nav.component.ts
+++ src/app/components/privileged-user/sidenav-nav/sidenav-nav.component.ts
@@ -1,20 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ChangeDetectorRef, OnDestroy} from '@angular/core';
 import {UserService} from '../../../services/userService/user.service';
 import {Router} from '@angular/router';
+import {MediaMatcher} from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-sidenav-nav',
   templateUrl: './sidenav-nav.component.html',
   styleUrls: ['./sidenav-nav.component.scss']
 })
-export class SidenavNavComponent implements OnInit {
+export class SidenavNavComponent implements OnInit, OnDestroy {
   public showNavigation: boolean;
-  constructor(private userService: UserService, private router: Router) {
+  public mobileQuery: MediaQueryList;
+
+  private readonly mobileQueryListener: () => void;
+
+  constructor(private userService: UserService, private router: Router, media: MediaMatcher, changeDetectorRef: ChangeDetectorRef) {
     this.showNavigation = userService.sessionIsActive();
-    this.router.events.subscribe((val) => this.showNavigation = userService.sessionIsActive());
+    this.router.events.subscribe(() => this.showNavigation = userService.sessionIsActive());
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this.mobileQueryListener);
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
   }
 
 }
